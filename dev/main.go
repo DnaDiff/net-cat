854f38c2ca1c0e068359bd677d110e91d4a63fde
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-12-11 19:13:26 +0200

    cleaning

--- dev/main.go
+++ dev/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
 )
 
 const (
-	port = "8080"
-	// maxMessageSize = 256
+	port           = "8080"
+	maxMessageSize = 256
 )
 
 func main() {
@@ -29,8 +32,29 @@ func main() {
 		}
 		// Handle the connection in a new goroutine
 		go handleConnection(conn)
-		// Send a message to the client
-		sendMessage(conn, "Nicec!\n")
+		// Send the pingu image to the client
+		PinguSender(conn)
+		// sendMessage(conn, "Pingu.txt\n")
+		sendMessage(conn, "NOOOT NOOOT!\n")
+	}
+}
+
+// Send the pingu image to the client
+func PinguSender(conn net.Conn) {
+	// get the contents of the pingu.txt file just like in go-reloaded
+	file, err := os.Open("../pingu.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		sendMessage(conn, scanner.Text()+"\n")
+	}
+	// check for errors
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -38,25 +62,27 @@ func handleConnection(conn net.Conn) {
 	// Do something with the connection
 	fmt.Println("Received a new connection!")
 	// Create a goroutine to read messages from the client
-	// go func() {
-	// 	for {
-	// 		// Read data from the connection
-	// 		data := make([]byte, maxMessageSize)
-	// 		_, err := conn.Read(data)
-	// 		if err != nil {
-	// 			// Handle the error
-	// 			fmt.Println(err)
-	// 			return
-	// 		}
-	// 		// Print the received message
-	// 		fmt.Println("Received message:", string(data))
-	// 	}
-	// }()
-	// Don't forget to close the connection when you're done
-	conn.Close()
+	go func() {
+		for {
+			readMessage(conn)
+		}
+	}()
+	// conn.Close()
 }
 
+func readMessage(conn net.Conn) {
+	// Read data from the connection
+	data := make([]byte, maxMessageSize)
+	_, err := conn.Read(data)
+	if err != nil {
+		// Handle the error
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Received message:", string(data))
+}
 func sendMessage(conn net.Conn, msg string) {
+	// Write data to the connection
 	_, err := conn.Write([]byte(msg))
 	if err != nil {
 		// Handle the error

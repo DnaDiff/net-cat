fe9d729c16ef6af2734473e991eb0ac5d009d265
Kevazy <25287241+Kevazy@users.noreply.github.com>
2022-12-12 13:51:41 +0200

    Message templates

---
+++ server/templates.go
@@ -0,0 +1,8 @@
+package server
+
+// Message templates for the client
+
+const (
+	welcomeMessage   = "Welcome to the TCP Chat!\nUsername: "
+	connectedMessage = "\033[H\033[2JWe are glad to have you here, %s!\nYou are now connected to the TCP Chat.\n"
+)

aead1490c41dbd92782465554877ce96384148f7
Kevazy <25287241+Kevazy@users.noreply.github.com>
2022-12-16 01:40:56 +0200

    Replaced strconv with homemade itoa

--- server/client.go
+++ server/client.go
@@ -3,7 +3,6 @@ package server
 import (
 	"fmt"
 	"net"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -108,5 +107,26 @@ func serverFull(clientList *ClientList, conn net.Conn) bool {
 func randomizeColor() string {
 	randSeed := time.Now().UnixNano()
 	randSeed = randSeed % int64(230) // int64 for more randomness due to UnixNano() being int64
-	return "\033[38;5;" + strconv.Itoa(int(randSeed)) + "m"
+	return "\033[38;5;" + itoa(int(randSeed)) + "m"
+}
+
+func itoa(n int) string {
+	result := ""
+	isNegative := ""
+
+	if n < 0 {
+		n *= 1
+		isNegative = "-"
+	}
+
+	if n == 0 {
+		return result
+	}
+
+	for n > 0 {
+		result = string(rune(n%10-'0')) + result
+		n /= 10
+	}
+
+	return isNegative + result
 }
